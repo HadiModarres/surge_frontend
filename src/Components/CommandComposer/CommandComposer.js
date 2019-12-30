e2712008b419135e3cc39cc64ecd103314db6fd0
Hadi Modarres <modarres.zadeh@gmail.com>
2019-12-30 13:20:35 +1100

    command composer structure

--- src/Components/CommandComposer/CommandComposer.js
+++ src/Components/CommandComposer/CommandComposer.js
@@ -2,16 +2,38 @@ import React from "react";
 import ArgPane from "./ArgPane";
 import OptionsPane from "./OptionsPane";
 import DescriptionPane from "./DescriptionPane";
+import Argument from "./Argument";
 
 class CommandComposer extends React.Component{
     constructor(props){
        super(props);
+        this.commandSelected = this.commandSelected.bind(this);
+        this.showOptions = this.showOptions.bind(this);
+        this.showDescription = this.showDescription.bind(this);
+        this.state= {options:[],description:""}
     }
+
+    commandSelected(command){
+
+    }
+    showOptions(options){
+
+    }
+    showDescription(description){
+
+    }
+
    render() {
        return (<div>
+               <Argument arg={{name:"command",displayName:"Command",valueType:"options",options:this.props.commands}} showOptions={this.showOptions} showDescription={this.showDescription}
+               valueSelected={this.commandSelected}/>
                <ArgPane/>
-               <OptionsPane></OptionsPane>
-               <DescriptionPane></DescriptionPane>
+               <OptionsPane>
+                   {this.state.options}
+               </OptionsPane>
+               <DescriptionPane>
+                   {this.state.description}
+               </DescriptionPane>
            </div>
        )
    }
