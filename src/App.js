07d46a3e79de14f845e9975536110fce1588643b
Hadi Modarres <modarres.zadeh@gmail.com>
2019-12-30 15:57:40 +1100

    command composer wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CommandComposer from "./Components/CommandComposer/CommandComposer";
+
+class DummyDataProvider {
+    getCommandList(){
+       return [
+           {name: "up",description: "Get percentage of positive close prices in a given period."},
+           {name: "avg",description: "Get average of close prices for given period"}
+       ]
+    }
+    symbolArg(){
+        return {name:"symbol",displayName:"symbol",description:"Specify stock's symbol. e.g. APT.AX",valueType:"string"}
+    }
+    pointsArg(){
+        return {name:"points",displayName:"point_count",description:"number of points to include in the calculation.",valueType:"number"}
+    }
+    getArgsForCommand(command){
+       switch (command.name) {
+           case "up":
+               return [this.symbolArg(),this.pointsArg()];
+           case "avg":
+               return [];
+           default:
+               return [];
+       }
+    }
+}
+
+let provider = new DummyDataProvider();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CommandComposer argsForCommand={(command)=>provider.getArgsForCommand(command)} commands={provider.getCommandList()}/>
     </div>
   );
 }
--- src/Components/CommandComposer/Argument.js
+++ src/Components/CommandComposer/Argument.js
@@ -8,7 +8,8 @@ class Argument extends React.Component{
          <FieldLabel displayName={this.props.displayName} showDescription={this.props.showDescription}
                      description={this.props.description}/>
          =
-         <FieldInput options={this.props.valueType==="options"?this.props.options:[]}/>
+         <FieldInput options={this.props.valueType==="options"?this.props.options:[]} showDescription={this.props.showDescription}
+         showOptions={this.props.showOptions}/>
       </div>)
    }
 }
