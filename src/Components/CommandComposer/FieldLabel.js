5cfe912ff198b6b44f268ef9bf2a9a3967801040
Hadi Modarres <modarres.zadeh@gmail.com>
2020-01-02 10:16:39 +1100

    command composer wip

--- src/Components/CommandComposer/FieldLabel.js
+++ src/Components/CommandComposer/FieldLabel.js
@@ -4,8 +4,14 @@ class FieldLabel extends React.Component{
     constructor(props){
        super(props);
     }
+    keyPressed(ev){
+        if (ev.key==='Enter'){
+            ev.target.click();
+            document.getElementsByClassName("arrow-navigable")[0].focus();
+        }
+    }
     render() {
-        return (<span className={"arrow-navigable"} onClick={this.props.onClick} tabIndex={"0"} onFocus={()=>{this.props.showDescription(this.props.description)}}>
+        return (<span className={"marg arrow-navigable"} onKeyPress={(ev)=>{this.keyPressed(ev)}} onClick={this.props.onClick} tabIndex={"0"} onFocus={()=>{this.props.showDescription(this.props.description)}}>
             {this.props.displayName}
         </span>);
     }
