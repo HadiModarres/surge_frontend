68995b0c850c287eb39c3a6d2a55ca40aba85fa9
Hadi Modarres <modarres.zadeh@gmail.com>
2020-01-07 16:34:05 +1100

    auth wip

--- src/Components/App/AuthenticatedApp.js
+++ src/Components/App/AuthenticatedApp.js
@@ -62,10 +62,10 @@ function getOptionalArgsForCommand(command) {
     }
 }
 
-function AuthenticatedApp() {
+function AuthenticatedApp(props) {
     return (
         <div className="App">
-            <button onClick={this.props.logout}>Logout</button>
+            <button onClick={props.logout}>Logout</button>
             <CommandComposer argsForCommand={(command) => getArgsForCommand(command)}
                              optionalArgsForCommand={(command) => getOptionalArgsForCommand(command)}
                              commands={getCommandList()}/>
