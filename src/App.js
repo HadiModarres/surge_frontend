07eb64e8fde5a960f24234188af246851cf6b5c6
Hadi Modarres <modarres.zadeh@gmail.com>
2020-01-07 16:21:06 +1100

    auth wip

--- src/App.js
+++ src/App.js
@@ -2,27 +2,20 @@ import React from "react";
 import AuthenticatedApp from "./Components/App/AuthenticatedApp";
 import UnauthenticatedApp from "./Components/App/UnauthenticatedApp";
 
-let api = require("./API/APIGateway");
 
 class App extends React.Component{
    constructor(props){
        super(props);
        this.state={loggedIn:false};
-       this.login = this.login.bind(this);
+       this.loggedIn = this.loggedIn.bind(this);
    }
 
-   async login(username,password){
-       try {
-           await api.login(username, password);
-           console.log("logged in");
-           // this.setState({loggedIn: true});
-       }catch (e) {
-           console.log(e);
-       }
+   loggedIn(){
+      this.setState({loggedIn:true});
    }
 
    render() {
-        return this.state.loggedIn ? <AuthenticatedApp/> : <UnauthenticatedApp login={this.login}/>
+        return this.state.loggedIn ? <AuthenticatedApp/> : <UnauthenticatedApp onLogin={this.loggedIn}/>
    }
 }
 
--- src/Components/App/UnauthenticatedApp.js
+++ src/Components/App/UnauthenticatedApp.js
@@ -1,15 +1,20 @@
 import React from "react";
+let api = require("../../API/APIGateway");
 
 class UnauthenticatedApp extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {username:"",password:""};
+        this.state = {username:"",password:"",error:null};
         this.loginClicked = this.loginClicked.bind(this);
     }
 
-    loginClicked(){
-        console.log(this.state.username, this.state.password);
-       this.props.login(this.state.username,this.state.password);
+    async loginClicked(){
+        try {
+            await api.login(this.state.username, this.state.password);
+            this.props.onLogin();
+        }catch (e) {
+            this.setState({error: e});
+        }
     }
 
     render() {
@@ -18,6 +23,7 @@ class UnauthenticatedApp extends React.Component{
                 Username: <input onChange={(ev)=>{this.setState({username:ev.target.value});}} value={this.state.username} type="text"/>
                 Password: <input onChange={(ev)=>{this.setState({password:ev.target.value});}} value={this.state.password} type="text"/>
                 <button onClick={this.loginClicked}>Login</button>
+                {this.state.error? <p>{this.state.error}</p>: null}
             </div>
         );
     }
